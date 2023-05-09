058bf7d6d1ee1b13c1ecb1524fd82d96e676c30b
Neakio <l.olivie@hotmail.com>
2023-05-09 16:48:50 +0200

    update serializer

---
+++ Frontend/src/components/pages/Grades/StudentGrade.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import ReactTable from "../../render-components/ReactTable";
+
+
+
+function GradesTable({ grades }) {
+    const columns = React.useMemo(
+        () => [
+            {
+                Header: "Course",
+                accessor: "title",
+            },
+            {
+                Header: "Lead Teacher",
+                accessor: "lead teacher",
+            },
+            {
+                Header: "Other Teachers",
+                accessor: "other teachers",
+            },
+            {
+                Header: "Grade",
+                accessor: "grade",
+            },
+            {
+                Header: "Comment",
+                accessor: "Comment",
+            },
+        ],
+        [],
+    );
+
+    return <ReactTable data={grades} columns={columns} />;
+}
+
+export default GradesTable;
