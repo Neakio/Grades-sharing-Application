f33492d3da544e5f7579a807b610154244e57006
Neakio <l.olivie@hotmail.com>
2023-05-17 09:36:26 +0200

    Merge commit

--- Frontend/src/components/pages/Grades/StudentTable.jsx
+++ Frontend/src/components/pages/Grades/StudentTable.jsx
@@ -4,11 +4,7 @@ import ReactTable from "../../render-components/ReactTable";
 
 
 
-<<<<<<< HEAD
-function GradesTable({ grades, courses }) {
-=======
 function StudentTable({ grades, courses }) {
->>>>>>> bc0518620206a0f8355490672af5944d29064a08
     const columns = React.useMemo(
         () => [
             {
@@ -38,8 +34,4 @@ function StudentTable({ grades, courses }) {
     return <ReactTable data={grades} columns={columns} />;
 }
 
-<<<<<<< HEAD
-export default GradesTable;
-=======
 export default StudentTable;
->>>>>>> bc0518620206a0f8355490672af5944d29064a08
--- Frontend/src/components/pages/Grades/TeacherGrade.jsx
+++ Frontend/src/components/pages/Grades/TeacherGrade.jsx
@@ -1,12 +1,7 @@
 import React, { useEffect, useState } from "react";
 
-<<<<<<< HEAD
-import ReactTable from "../../render-components/ReactTable";
-import { getCourses, getGrades } from "../../../services/api";
-=======
 import { getCoursesByTeacher, getGrades } from "../../../services/api";
 import TeacherTable from "./TeacherTable";
->>>>>>> bc0518620206a0f8355490672af5944d29064a08
 
 
 
@@ -21,16 +16,6 @@ function TeacherView() {
         fetchCourses();
     }, []);
 
-<<<<<<< HEAD
-    const fetchGrades = async () => {
-        let grades = await getGrades(courses.Id);
-        setGrades(grades);
-    };
-    const fetchCourses = async () => {
-        let courses = await getCourses();
-        setCourses(courses);
-    }
-=======
     const fetchCourses = async () => {
         let courses = await getCoursesByTeacher(id);
         setCourses(courses);
@@ -40,46 +25,11 @@ function TeacherView() {
         let grades = await getGrades(course.Id);
         setGrades(grades);
     };
->>>>>>> bc0518620206a0f8355490672af5944d29064a08
 
 
 
 
-<<<<<<< HEAD
-    const columns = React.useMemo(
-        () => [
-            {
-                Header: "Student",
-                accessor: "student",
-            },
-            {
-                Header: "Course",
-                accessor: "title",
-            },
-            {
-                Header: "Lead Teacher",
-                accessor: "lead teacher",
-            },
-            {
-                Header: "Other Teachers",
-                accessor: "other teachers",
-            },
-            {
-                Header: "Grade",
-                accessor: "grade",
-            },
-            {
-                Header: "Comment",
-                accessor: "Comment",
-            },
-        ],
-        [],
-    );
-
-    return <ReactTable data={grades} columns={columns} />;
-=======
     return <TeacherTable grades={grades} courses={courses} />;
->>>>>>> bc0518620206a0f8355490672af5944d29064a08
 }
 
 export default TeacherView;
