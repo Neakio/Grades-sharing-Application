68ef29dc90b6b4e740733ac47340de2e31367f75
Neakio <l.olivie@hotmail.com>
2023-05-17 09:36:24 +0200

    Merge branch

--- Frontend/src/components/pages/Grades/StudentGrade.jsx
+++ Frontend/src/components/pages/Grades/StudentGrade.jsx
@@ -1,24 +1,4 @@
 import React, { useEffect, useState } from "react";
-<<<<<<< HEAD
-import { useParams } from "react-router-dom";
-
-import GradesTable from "./StudentTable";
-import { getGrades } from "../../../services/api";
-
-function StudentView({courses}) {
-    const [grades, setGrades] = useState([])
-    const { id } = useParams();
-    
-    useEffect(() => {
-        fetchGrades();
-    }, []);
-
-    const fetchGrades = async () => {
-        let grades = await getGrades(id);
-        setGrades(grades);
-    };
-    return <GradesTable grades={grades} courses={courses}/>;
-=======
 
 import StudentTable from "./StudentTable";
 import { getCourses, getGrades } from "../../../services/api";
@@ -41,7 +21,6 @@ function StudentView() {
         setGrades(grades);
     };
     return <StudentTable grades={grades} courses={courses}/>;
->>>>>>> bc0518620206a0f8355490672af5944d29064a08
 }
 
 export default StudentView;
