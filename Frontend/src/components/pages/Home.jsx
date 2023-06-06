15a3c5f4467b3aae47dae26961360029617168e1
LucileOLIVIE <lucile.olivie@limayrac.fr>
2023-06-06 21:25:32 +0200

    added quotes

--- Frontend/src/components/pages/Home.jsx
+++ Frontend/src/components/pages/Home.jsx
@@ -1,36 +1,48 @@
 import React, { Fragment, useEffect, useState } from "react";
-import axios from "axios";
+import quotesData from "./quotes.json";
 
 function Home({ isLoggedIn }) {
-    const [quote, setQuote] = useState({});
+    const [randomQuote, setRandomQuote] = useState(null);
 
     useEffect(() => {
-        fetchQuote();
+        generateRandomQuote();
     }, []);
-    const fetchQuote = async () => {
-        let response = axios.get("https://zenquotes.io/api/random");
-        setQuote(response[0]);
-    };
+
+    function generateRandomQuote() {
+        const randomIndex = Math.floor(Math.random() * quotesData.length);
+        const quote = quotesData[randomIndex];
+        setRandomQuote(quote);
+    }
     return (
         <Fragment>
             {isLoggedIn ? (
                 <Fragment>
                     <div>
-                        <p>
-                            <i>{quote.q}</i>, <b>{quote.a}</b>
-                        </p>
-                    </div>
-                    <div>
-                        <h1>App Context</h1>
-                        <center>
-                            <p>
-                                Currently an excel file is used to share grades with the students.
-                                In order to modernize this process this web application has been
-                                created. Single Sign On for students / teachers / the administration
-                                is implemented to log in to the app and is redundant in terms of
-                                failures.
-                            </p>
-                        </center>
+                        {randomQuote ? (
+                            <div className="quote-container">
+                                <blockquote className="quote">
+                                    <i>&ldquo;{randomQuote.q}&rdquo;</i>
+                                </blockquote>{" "}
+                                <br />
+                                <p className="author">
+                                    <b>&mdash; {randomQuote.a}</b>
+                                </p>
+                            </div>
+                        ) : (
+                            <p>Loading...</p>
+                        )}
+                        <div>
+                            <h1>App Context</h1>
+                            <center>
+                                <p>
+                                    Currently an excel file is used to share grades with the
+                                    students. In order to modernize this process this web
+                                    application has been created. Single Sign On for students /
+                                    teachers / the administration is implemented to log in to the
+                                    app and is redundant in terms of failures.
+                                </p>
+                            </center>
+                        </div>
                     </div>
                 </Fragment>
             ) : (
